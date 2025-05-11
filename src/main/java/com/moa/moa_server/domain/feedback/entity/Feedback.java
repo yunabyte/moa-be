6809ee15ba3ce39a5e99fa32e21645fff430f20f
Jiwon Son <jiwonsoong13@gmail.com>
2025-05-11 16:49:42 +0900

    [Feat] 사용자 피드백 등록 API 개발 (#78)
    * feat: feedback 엔티티 추가 (#77)
    
    * feat: 피드백 등록 요청 dto 추가 (#77)
    
    * feat: feedback 리포지토리 추가 (#77)
    
    * feat: 피드백 등록 서비스 구현 (#77)
    
    * feat: 피드백 등록 컨트롤러 구현 (#77)
    
    * feat: 피드백 본문 유효성 검사 추가 (#77)

---
+++ src/main/java/com/moa/moa_server/domain/feedback/entity/Feedback.java
@@ -0,0 +1,33 @@
+package com.moa.moa_server.domain.feedback.entity;
+
+import com.moa.moa_server.domain.global.entity.BaseTimeEntity;
+import com.moa.moa_server.domain.user.entity.User;
+import jakarta.persistence.*;
+import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@AllArgsConstructor
+@Builder
+public class Feedback extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Column(length = 500, nullable = false)
+    private String content;
+
+    public static Feedback create(User user, String content) {
+        return Feedback.builder()
+                .user(user)
+                .content(content)
+                .build();
+    }
+}
---
+++ src/main/java/com/moa/moa_server/domain/feedback/handler/FeedbackException.java
@@ -0,0 +1,8 @@
+package com.moa.moa_server.domain.feedback.handler;
+
+import com.moa.moa_server.domain.global.exception.BaseErrorCode;
+import com.moa.moa_server.domain.global.exception.BaseException;
+
+public class FeedbackException extends BaseException {
+    public FeedbackException(BaseErrorCode errorCode) { super(errorCode); }
+}
